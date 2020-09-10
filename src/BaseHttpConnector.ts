e9b4ef6b4a66e108923ea683fc53fdb74da194fa
christophe gachiniard <christophe@reshuffle.com>
2020-09-10 17:16:46 +1200

    App is mandatory in connectors constructor

--- src/BaseHttpConnector.ts
+++ src/BaseHttpConnector.ts
@@ -1,12 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
-import { Reshuffle } from './types'
 import { BaseConnector } from './'
 
 class BaseHttpConnector<ConfigOptionsType = any, EventOptionsType = any> extends BaseConnector {
-  constructor(app: Reshuffle, options?: ConfigOptionsType, id?: string) {
-    super(app, options, id)
-  }
-
   async handle(req: Request, res: Response, next: NextFunction) {
     throw new Error('handle function needs to be implemented in your Http connector implementation')
     return false
